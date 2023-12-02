dd07aca32b1e3c4b23feb06dac234f23c23ca1f4
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-12-03 02:56:06 +0530

    Fix bugs in PlaneLogic.cs

--- AR Demo/Assets/Scripts/PlaneLogic.cs
+++ AR Demo/Assets/Scripts/PlaneLogic.cs
@@ -2,60 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 
 public class PlaneLogic : MonoBehaviour
 {
     [SerializeField] private Toggle Vertical, Horizontal, GamePrepare;
     [SerializeField] private Button startButton;
+    [SerializeField] private GameObject uiPanel; // Reference to the UI panel
 
     private ARPlaneDetect arPlaneDetect;
+    private ARPlaneManager planeManager;
+    private ARTaptoPlace arTapToPlace;
 
-    public bool VerticalPlaneToggle 
-    { 
-        get=> Vertical.isOn;
+    private bool verticalFound = false;
+    private bool horizontalFound = false;
+
+    private void Start()
+    {
+        arPlaneDetect = FindAnyObjectByType<ARPlaneDetect>();
+        arPlaneDetect.OnVerticalPlaneFound += () => { VerticalPlaneToggle = true; };
+        arPlaneDetect.OnHorizontalPlaneFound += () => { HorizontalPlaneToggle = true; };
+        arPlaneDetect.OnGamePrepare += () => { GamePrepareToggle = true; };
+
+        planeManager = FindObjectOfType<ARPlaneManager>();
+        arTapToPlace = FindObjectOfType<ARTaptoPlace>();
+        arTapToPlace.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        startButton.onClick.AddListener(OnStartButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        startButton.onClick.RemoveListener(OnStartButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        uiPanel.SetActive(false); // Hide the UI panel
+
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+
+        arTapToPlace.enabled = true;
+        startButton.interactable = false;
+    }
+
+    public bool VerticalPlaneToggle
+    {
+        get => Vertical.isOn;
         set
         {
             Vertical.isOn = value;
-            checkAll();
-        } 
+            verticalFound = value;
+            CheckAllPlanesFound();
+        }
     }
+
     public bool HorizontalPlaneToggle
     {
         get => Horizontal.isOn;
         set
         {
             Horizontal.isOn = value;
-            checkAll();
+            horizontalFound = value;
+            CheckAllPlanesFound();
         }
     }
+
     public bool GamePrepareToggle
     {
         get => GamePrepare.isOn;
         set
         {
             GamePrepare.isOn = value;
-            checkAll();
+            CheckAllPlanesFound();
         }
     }
 
-    private void OnEnable()
+    private void CheckAllPlanesFound()
     {
-        arPlaneDetect = FindAnyObjectByType<ARPlaneDetect>();
-        arPlaneDetect.OnVerticalPlaneFound += () => VerticalPlaneToggle = true;
-        arPlaneDetect.OnHorizontalPlaneFound += () => HorizontalPlaneToggle = true;
-        arPlaneDetect.OnGamePrepare += () => GamePrepareToggle = true;
-    }
-
-    private void OnDisable()
-    {
-        arPlaneDetect.OnVerticalPlaneFound -= () => VerticalPlaneToggle = true;
-        arPlaneDetect.OnHorizontalPlaneFound -= () => HorizontalPlaneToggle = true;
-        arPlaneDetect.OnGamePrepare -= () => GamePrepareToggle = true;
-    }
-
-    private void checkAll()
-    {
-        if (Vertical && Horizontal && GamePrepare)
+        if (verticalFound && horizontalFound && GamePrepare.isOn)
         {
             startButton.interactable = true;
         }
