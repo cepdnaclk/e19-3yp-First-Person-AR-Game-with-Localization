b36b1462e23d61567bbf1b8eeeaba9450dc321dc
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-12-03 02:14:48 +0530

    Add a screen for players to get ready

--- AR Demo/Assets/Scripts/ARPlaneDetect.cs
+++ AR Demo/Assets/Scripts/ARPlaneDetect.cs
@@ -1,18 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARPlaneDetect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private ARPlaneManager m_planeManager;
+    private List<ARPlane> m_planeList;
+
+    public event Action OnVerticalPlaneFound;
+    public event Action OnHorizontalPlaneFound;
+    public event Action OnGamePrepare;
+
+    private bool Vertical = false, Horizontal = false;
+
+    private void OnEnable()
     {
         
+        m_planeList = new List<ARPlane>();
+        m_planeManager = FindObjectOfType<ARPlaneManager>();
+        m_planeManager.planesChanged += OnPlanesChanged;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
         
+        m_planeManager.planesChanged -= OnPlanesChanged;
+
     }
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        if(args.added != null && args.added.Count > 0)
+        {
+            m_planeList.AddRange(args.added);
+        }
+
+        foreach(ARPlane plane in m_planeList.Where(plane=>plane.extents.x*plane.extents.y>=0.1f)) {
+            if (plane.alignment.IsVertical()) 
+            {
+                OnVerticalPlaneFound.Invoke();
+                Vertical = true;
+            }
+            else 
+            {
+                OnHorizontalPlaneFound.Invoke();
+                Horizontal = true;
+            }
+
+            if(Vertical && Horizontal)
+            {
+                OnGamePrepare.Invoke();
+            }
+            
+        }
+    }
+
 }
--- AR Demo/Assets/Scripts/PlaneLogic.cs
+++ AR Demo/Assets/Scripts/PlaneLogic.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaneLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Toggle Vertical, Horizontal, GamePrepare;
+    [SerializeField] private Button startButton;
+
+    private ARPlaneDetect arPlaneDetect;
+
+    public bool VerticalPlaneToggle 
+    { 
+        get=> Vertical.isOn;
+        set
+        {
+            Vertical.isOn = value;
+            checkAll();
+        } 
+    }
+    public bool HorizontalPlaneToggle
     {
-        
+        get => Horizontal.isOn;
+        set
+        {
+            Horizontal.isOn = value;
+            checkAll();
+        }
+    }
+    public bool GamePrepareToggle
+    {
+        get => GamePrepare.isOn;
+        set
+        {
+            GamePrepare.isOn = value;
+            checkAll();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        arPlaneDetect = FindAnyObjectByType<ARPlaneDetect>();
+        arPlaneDetect.OnVerticalPlaneFound += () => VerticalPlaneToggle = true;
+        arPlaneDetect.OnHorizontalPlaneFound += () => HorizontalPlaneToggle = true;
+        arPlaneDetect.OnGamePrepare += () => GamePrepareToggle = true;
+    }
+
+    private void OnDisable()
+    {
+        arPlaneDetect.OnVerticalPlaneFound -= () => VerticalPlaneToggle = true;
+        arPlaneDetect.OnHorizontalPlaneFound -= () => HorizontalPlaneToggle = true;
+        arPlaneDetect.OnGamePrepare -= () => GamePrepareToggle = true;
+    }
+
+    private void checkAll()
+    {
+        if (Vertical && Horizontal && GamePrepare)
+        {
+            startButton.interactable = true;
+        }
     }
 }
