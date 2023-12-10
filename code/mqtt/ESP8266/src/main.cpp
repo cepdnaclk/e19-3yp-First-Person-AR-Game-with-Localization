ce286665df2ded255ba899ff48bad1914c07135b
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2023-12-10 13:28:52 +0530

    create led indicator to connect wifi and mqtt

--- code/mqtt/ESP8266/src/main.cpp
+++ code/mqtt/ESP8266/src/main.cpp
@@ -1,28 +1,19 @@
-// #include <Arduino.h>
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
  
-const char* ssid = "Dasun's Galaxy M21"; // Enter your WiFi name
-const char* password =  "sise9444"; // Enter WiFi password
-const char* mqttServer = "192.168.182.122";
-const int mqttPort = 1883;
+const char* SSID = "Dasun's Galaxy M21";    // WiFi name
+const char* PASSWORD =  "sise9444";         // WiFi password
+const char* ID = "nodemcu01";               // Device ID
+const char* TOPIC = "node/nodemcu01";
+const char* NODESTATUS = "node/status";
+const char* mqttServer = "192.168.182.122"; // Broker IP
+const int mqttPort = 1883;                  // Broker port
+
+int WIFI_LED = 2;      // Assign LED1 to pin GPIO2
+int MQTT_LED = 16;     // Assign LED1 to pin GPIO16
+
+unsigned long lasttime = 0;
+
 // const char* mqttUser = "otfxknod";
 // const char* mqttPassword = "nSuUc1dDLygF";
  
@@ -30,66 +21,86 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-    for (unsigned int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void wifi_connect(){
+  analogWrite(WIFI_LED, 512);
+ 
+  WiFi.begin(SSID, PASSWORD);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  analogWrite(WIFI_LED, 0);
+}
+
+void mqtt_connect() {
+  analogWrite(MQTT_LED, 512);
+
+  // while (!client.connected()) {
+    if (client.connect(ID)) {
+      analogWrite(MQTT_LED, 0);
+
+      client.publish(NODESTATUS, "1"); // Update Node Status
+      client.subscribe(TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
     }
-    Serial.println();
+  // }
 }
  
 void setup() {
  
   Serial.begin(115200);
+
+  // initialize GPIO2 and GPIO16 as an output
+  pinMode(WIFI_LED, OUTPUT);
+  pinMode(MQTT_LED, OUTPUT);
+
+  analogWrite(WIFI_LED, 512);
+  analogWrite(MQTT_LED, 512);
+
+  delay(500);
  
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
+  wifi_connect();
  
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
  
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
- 
-    if (client.connect("ESP8266Client" )) {
- 
-      Serial.println("connected");  
- 
-    } else {
- 
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
- 
-    }
-  }
- 
-  client.publish("python/mqtt", "hello"); //Topic name
-  client.subscribe("python/mqtt");
- 
+  mqtt_connect();
+
 }
  
-// void callback(char* topic, byte* payload, unsigned int length) {
- 
-//   Serial.print("Message arrived in topic: ");
-//   Serial.println(topic);
- 
-//   Serial.print("Message:");
-//   for (int i = 0; i < length; i++) {
-//     Serial.print((char)payload[i]);
-//   }
- 
-//   Serial.println();
-//   Serial.println("-----------------------");
- 
-// }
- 
 void loop() {
-  client.loop();
+  if (WiFi.status() != WL_CONNECTED) {
+    wifi_connect();
+  }
+  if (!client.connected()) {
+    mqtt_connect();
+  }else{
+    client.loop();
+
+    unsigned long now = millis();
+    if (now - lasttime > 1000) {
+      lasttime = now;
+      client.publish(NODESTATUS, "1");
+      analogWrite(WIFI_LED, 512);
+      analogWrite(MQTT_LED, 512);
+    }
+    if (now - lasttime > 500) {
+      analogWrite(WIFI_LED, 0);
+      analogWrite(MQTT_LED, 0);
+    }
+  }
 }
