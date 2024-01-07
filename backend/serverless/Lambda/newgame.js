0a5969a3e1faec9a6d30e2869aad857e92df9263
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-07 23:47:46 +0530

    new game lambda debugged and tested

--- backend/serverless/Lambda/newgame.js
+++ backend/serverless/Lambda/newgame.js
@@ -1,30 +1,30 @@
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient()
+
+const client = new DynamoDBClient();
 
 exports.handler = async (event, context, callback) => {
     try {
-        
         const requestBody = event;
         const email = requestBody.email;
         const users = requestBody.users;
         const stationid = requestBody.stationid;
+
         const dynamoDBParamsEnv = {
-            "TableName": 'Arcombat-env',
-            "Item": {
-                "email": {"S": email},
-                "users": {"L":users},
-                "stationid": {"L":stationid}
-                           
+            TableName: 'Arcombat-env',
+            Item: {
+                email: { S: email },
+                users: { L: users.map(user => ({ S: user })) }, // Assuming users is an array of strings
+                stationid: { L: stationid.map(id => ({ S: id})) } // Assuming stationid is an array of numbers
             }
         };
+
         const commandEnv = new PutItemCommand(dynamoDBParamsEnv);
         const responsedbEnv = await client.send(commandEnv);
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'new game created successfully' }),
+            body: { message: 'new game created successfully' },
         };
-
-    //error handleing
     } catch (error) {
         console.error('Error:', error);
         return {
