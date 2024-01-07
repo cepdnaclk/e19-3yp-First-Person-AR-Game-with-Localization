8ab5455eb0a49668d0540ad557b7f5cb2d3ec410
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-07 22:54:36 +0530

    new game functionality added - need to be tested

--- backend/serverless/Lambda/newgame.js
+++ backend/serverless/Lambda/newgame.js
@@ -6,7 +6,24 @@ exports.handler = async (event, context, callback) => {
         
         const requestBody = event;
         const email = requestBody.email;
-        
+        const users = requestBody.users;
+        const stationid = requestBody.stationid;
+        const dynamoDBParamsEnv = {
+            "TableName": 'Arcombat-env',
+            "Item": {
+                "email": {"S": email},
+                "users": {"L":users},
+                "stationid": {"L":stationid}
+                           
+            }
+        };
+        const commandEnv = new PutItemCommand(dynamoDBParamsEnv);
+        const responsedbEnv = await client.send(commandEnv);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'new game created successfully' }),
+        };
+
     //error handleing
     } catch (error) {
         console.error('Error:', error);
