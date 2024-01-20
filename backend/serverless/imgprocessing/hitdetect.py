cd87b95a2fa73d1004528ae6caaeef66c21848c0
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-20 22:11:08 +0530

    add hsv  min and max for red

--- backend/serverless/imgprocessing/hitdetect.py
+++ backend/serverless/imgprocessing/hitdetect.py
@@ -3,4 +3,10 @@ import numpy as np
 
 img = cv2.imread("test")
 cv2.imshow("test",img)
+
+#hsv min and max values
+lower_red = np.array([0, 120, 70]) 
+upper_red = np.array([10, 255, 255]) 
+
+
 cv2.waitKey(0)
