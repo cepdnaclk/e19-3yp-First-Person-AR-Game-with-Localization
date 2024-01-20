155462a932952a3c3990046186dcdc437b51524a
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-20 15:12:45 +0530

    socket msg tested 10 min idle time - hardcoded connection ids

--- backend/serverless/Lambda/socketmsg.js
+++ backend/serverless/Lambda/socketmsg.js
@@ -1,32 +1,28 @@
-import AWS from 'aws-sdk';
+const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require("@aws-sdk/client-apigatewaymanagementapi");
 
-const ENDPOINT = '9c0zh8p4oj.execute-api.ap-southeast-1.amazonaws.com/beta/';
-const client = new AWS.ApiGatewayManagementApi({ endpoint: ENDPOINT });
+const ENDPOINT = 'https://9c0zh8p4oj.execute-api.ap-southeast-1.amazonaws.com/beta/';
+const client = new ApiGatewayManagementApiClient({ endpoint: ENDPOINT });
 
-const sendToOne = async (id, body) => {
-  try {
-    await client.postToConnection({
-      'ConnectionId': id,
-      'Data': Buffer.from(JSON.stringify(body)),
-    }).promise();
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event, context) => {
     try {
-        await sendToOne('R02GBdCDSQ0CGgA=', { message: 'hello' });
+        const input = {
+            Data: "hello hhj",
+            ConnectionId: 'R1QipefWSQ0CEUQ=', 
+        };
+
+        const command = new PostToConnectionCommand(input);
+        await client.send(command);
+
         return {
             statusCode: 200,
             body: JSON.stringify('Connection started successfully.')
         };
+    } catch (error) {
+        console.error('Error:', error);
 
-    }catch (error) {
         return {
             statusCode: 500,
             body: JSON.stringify('Error starting connection.')
         };
-
     }
-}
+};
