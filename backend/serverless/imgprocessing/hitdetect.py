c6d9e5d0b50679113427943997732da1d30f8d58
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-21 13:34:51 +0530

    detect cross bar and decode qr and detect hit

--- backend/serverless/imgprocessing/hitdetect.py
+++ backend/serverless/imgprocessing/hitdetect.py
@@ -1,18 +1,22 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("test4")
+img = cv2.imread("qr.jpg")
 cv2.imshow("test",img)
 imgResult = img.copy()
 
+qcd = cv2.QRCodeDetector()
+retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
+#print(points)
+#print(decoded_info)
 def get_contours(img):
     contours,hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     x,y,w,h = 0,0,0,0
     #print(contours)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        print(area)
-        if area>15:
+        #print(area)
+        if area>2:
             cv2.drawContours(imgResult, cnt, -1, (0, 255, 0), 6)
             peri = cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)
@@ -22,10 +26,15 @@ def get_contours(img):
 
 def findColor():
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0, 120, 70])
+    lower_red = np.array([0, 50, 50])
     upper_red = np.array([10, 255, 255])
     mask = cv2.inRange(imgHSV, lower_red, upper_red)
     x, y = get_contours(mask)
+    #print(points)
+    for qrs_id in range(len(points)):
+        if points[qrs_id][0][0] <=x and points[qrs_id][1][0]>=x:
+            if points[qrs_id][0][1] <=y and points[qrs_id][2][1]>=y:
+                print("hit", decoded_info[qrs_id])
 
 findColor()
 
