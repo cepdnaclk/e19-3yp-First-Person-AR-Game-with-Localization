639a1e5c96276ec72ce7bce746e7f2610f79e9dc
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-26 00:40:16 +0530

    build socket json msg

--- backend/serverless/Lambda/opencvsocketmsg.js
+++ backend/serverless/Lambda/opencvsocketmsg.js
@@ -1,15 +1,48 @@
 const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require("@aws-sdk/client-apigatewaymanagementapi");
+const { DynamoDBClient,  GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const dbclient = new DynamoDBClient()
 
 const ENDPOINT = 'https://9c0zh8p4oj.execute-api.ap-southeast-1.amazonaws.com/beta/';
 const client = new ApiGatewayManagementApiClient({ endpoint: ENDPOINT });
 
 exports.handler = async (event, context) => {
     try {
+        const opencvresult = JSON.parse(event)
+        const hit = opencvresult.hit
+        const shooter = opencvresult.shooter
+
+        const hitterIdParams = {
+            "TableName": 'Arcombat-socket',
+            "Key": {
+                "email": {"S": hit},
+                
+            }
+        };
+
+        const shooterIdParams = {
+            "TableName": 'Arcombat-socket',
+            "Key": {
+                "email": {"S": shooter},
+                
+            }
+        };
+
         console.log(event);
-        const input = {
-            Data: "hello hhj",
+        const hitMsg = {
+            Data: JSON.stringify({
+                "hit": "1",
+                "shoot": "0"
+            }),
             ConnectionId: 'R1QipefWSQ0CEUQ=', // Replace with your actual ConnectionId
         };
+        const shootMsg = {
+            Data: JSON.stringify({
+                "hit": "0",
+                "shoot": "1"
+            }),
+            ConnectionId: 'R1QipefWSQ0CEUQ=', // Replace with your actual ConnectionId
+        };
+
 
         const command = new PostToConnectionCommand(input);
         await client.send(command);
