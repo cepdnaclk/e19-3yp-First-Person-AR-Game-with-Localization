ae88521724d6848f3f4a06643a011b7aeb19739d
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-26 01:37:23 +0530

    fix bugs of opencv socket

--- backend/serverless/Lambda/opencvsocketmsg.js
+++ backend/serverless/Lambda/opencvsocketmsg.js
@@ -3,13 +3,16 @@ const { DynamoDBClient,  GetItemCommand } = require("@aws-sdk/client-dynamodb");
 const dbclient = new DynamoDBClient()
 
 const ENDPOINT = 'https://9c0zh8p4oj.execute-api.ap-southeast-1.amazonaws.com/beta/';
+
 const client = new ApiGatewayManagementApiClient({ endpoint: ENDPOINT });
 
 exports.handler = async (event, context) => {
     try {
-        const opencvresult = JSON.parse(event)
+        const opencvresult = event
         const hit = opencvresult.hit
         const shooter = opencvresult.shooter
+        console.log(hit)
+        console.log(shooter)
 
         const hitterIdParams = {
             "TableName": 'Arcombat-socket',
@@ -27,32 +30,34 @@ exports.handler = async (event, context) => {
             }
         };
         const commandHitId = new GetItemCommand(hitterIdParams);
-        const responseHitId = await client.send(commandHitId);
-        const hitterId = response.Item.connectionid.S;
+        const responseHitId = await dbclient.send(commandHitId);
+        const hitterId = responseHitId.Item.connectionid.S;
 
         const commandShootId = new GetItemCommand(shooterIdParams);
-        const responseShootId = await client.send(commandShootId);
-        const shooterId = response.Item.connectionid.S;
-
-        console.log(event);
+        const responseShootId = await dbclient.send(commandShootId);
+        const shooterId = responseShootId.Item.connectionid.S;
+        console.log(hitterId)
+        console.log(shooterId)
+        // {
+        //     "hit": "1",
+        //     "shoot": "0"
+        // },
+        // console.log(event);
         const hitMsg = {
-            Data: JSON.stringify({
-                "hit": "1",
-                "shoot": "0"
-            }),
+            Data: "hit",
             ConnectionId: hitterId,
         };
         const shootMsg = {
-            Data: JSON.stringify({
-                "hit": "0",
-                "shoot": "1"
-            }),
+            Data:"score",
             ConnectionId: shooterId,
         };
 
 
-        const command = new PostToConnectionCommand(input);
-        await client.send(command);
+        const commandSendHitter = new PostToConnectionCommand(hitMsg);
+        await client.send(commandSendHitter);
+
+        const commandSendShooter = new PostToConnectionCommand(shootMsg);
+        await client.send(commandSendShooter);
 
         return {
             statusCode: 200,
