e84798acc88f2828e5cabccd2dab28daa122ab1a
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-01-25 16:13:34 +0530

    Can successfully host and retrieve anchors automatically

--- AR Demo/Assets/Scripts/JoinGame.cs
+++ AR Demo/Assets/Scripts/JoinGame.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Google.XR.ARCoreExtensions.Samples.PersistentCloudAnchors;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
-
+using UnityEngine.SceneManagement;
 
 public class Player {
     public string email;
@@ -19,6 +20,18 @@ public class JoinGame : MonoBehaviour
     private string accessToken;
     string gameID;
     string JoinGameEndpoint = "https://z760hx70mc.execute-api.ap-southeast-1.amazonaws.com/beta/joingame";
+    public string cloudID;
+
+    public delegate void ApiCallback(string response);
+
+    private ResolveMenuManager resolveMenuManager;
+
+    // Callback function variable
+    public ApiCallback onApiCallback;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -73,7 +86,28 @@ public class JoinGame : MonoBehaviour
         {
             Debug.Log("Success");
             string responseText = JoinGameRequest.downloadHandler.text;
+            // Call the callback function with the API response
+            cloudID = setCloudID(responseText);
+            Debug.Log("CloudID " + cloudID);
             Debug.Log("API Response: " + responseText);
+            PlayerPrefs.SetString("RetreivedID",responseText);
+            
+
+            if (onApiCallback != null)
+            {
+                onApiCallback(responseText);
+            }
+            SceneManager.LoadScene(7);
         }
     }
+    
+    public string setCloudID(string cloudID) 
+    {
+        
+            return cloudID;
+        
+    }
+
 }
+
+
