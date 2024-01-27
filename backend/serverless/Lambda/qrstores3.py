4b5e40c925fd704c8cd7c21fac5ed9ca550cd3cb
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-27 18:19:17 +0530

    qr store tested

--- backend/serverless/Lambda/qrstores3.py
+++ backend/serverless/Lambda/qrstores3.py
@@ -1,5 +1,8 @@
 import qrcode
 import io
+import boto3
+
+client = boto3.client('s3')
 
 
 # Generate QR code
@@ -9,8 +12,10 @@ qr = qrcode.QRCode(
     box_size=50,
     border=2
 )
+
+bucket_name = 'arcombat-qr'
 def lambda_handler(event, context):
-    email = event
+    email = event['email']
     qr.add_data(email)
     qr.make(fit=True)
 
@@ -19,3 +24,6 @@ def lambda_handler(event, context):
     img_bytes_io = io.BytesIO()
     img.save(img_bytes_io)
     img_bytes = img_bytes_io.getvalue()
+
+    client.put_object(Bucket=bucket_name, Key=email+'.png', Body=img_bytes)
+    return "success"
