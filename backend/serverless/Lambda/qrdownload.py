22ae03032d090c94b21b2bb1450496b400892df4
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-01-27 18:37:30 +0530

    qr download debugged

--- backend/serverless/Lambda/qrdownload.py
+++ backend/serverless/Lambda/qrdownload.py
@@ -6,13 +6,13 @@ client=boto3.client('s3')
 def lambda_handler(event, context):
 
     bucket_name='arcombat-qr'
-    image_file_name=event['body']['email']
+    body_dict = json.loads(event['body'])
+    image_file_name=body_dict['email']
    
     response = client.get_object(
     Bucket=bucket_name,
     Key=image_file_name+'.png',
 )
-    print("Response from s3 : ",response)
     image_file_to_be_downloaded=response['Body'].read()
     return {
         'statusCode': 200,
